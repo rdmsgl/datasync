7347cb10f91b117b83e159827d683e01717ab43b
Randall D. Miles <rdmsgl@gmail.com>
2015-03-22 16:00:04 -0400

    Added initial read record routine.

--- datarecord.cpp
+++ datarecord.cpp
@@ -6,7 +6,7 @@ public :
 private :
 	int numFields;
 	std::vector<std::string> fields;
-	std::vector<int> id;
+	std::vector<int> syncFieldNum;
 }
 
 
@@ -17,6 +17,27 @@ public :
 	void writeFile(std::string fname);
         void addColumn(dataRecordSet recSet, int col);
 private :
+        char recSeparator=",";
 	int numRecords;
+	std::vector<std::string> columnLabels;
 	std::vector<dataRecord> records;
+	std::string readRecord(std::stringstream input);
 }
+
+
+void dataRecord::addField(std::string value)
+{
+	fields.push_back(value);
+        numFields=fields.size();
+}
+
+std::string dataRecordSet::readRecord(std::stringstream input)
+{
+	std::string rec;
+	rec.resize(input.size());
+	auto c=rec.begin();
+        *c=input.getc();
+	while (*c!=",") *(c++)=input.getc();
+	rec.resize(c-rec.begin());
+}
+
